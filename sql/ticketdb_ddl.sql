78b88f5371bc00ec183bb7ad2486da65a83dd1f3
SungKyu Sim <simsungkyu@gmail.com>
2014-04-28 14:54:17 +0900

    ddl update - add notification table and constraints

--- sql/ticketdb_ddl.sql
+++ sql/ticketdb_ddl.sql
@@ -3,6 +3,7 @@
 CREATE TABLE `user` ( 
 	`id`     bigint(20) AUTO_INCREMENT NOT NULL,
 	`userid` varchar(128) NOT NULL,
+	`username` varchar(128) NOT NULL,
 	`userpwd`	varchar(128) NOT NULL,
 	PRIMARY KEY(`id`)
 );
@@ -28,24 +29,44 @@ CREATE TABLE `item` (
 
 CREATE TABLE `cart` ( 
 	`id`       bigint(20) NOT NULL,
-	`empid`    	bigint(20) NOT NULL,
-	`userid`   bigint(20) NOT NULL,
+	`emp_id`    	bigint(20) NOT NULL,
+	`user_id`   bigint(20) NOT NULL,
 	`msg`      	varchar(1024) NULL,
 	`timestamp`	timestamp NULL ,
 	PRIMARY KEY(`id`)
 );
 
 ALTER TABLE `cart` ADD CONSTRAINT `cart_user_fk` 
-	FOREIGN KEY(`userid`) REFERENCES `user`(`id`);
+	FOREIGN KEY(`user_id`) REFERENCES `user`(`id`);
 
 ALTER TABLE `cart` ADD CONSTRAINT `cart_emp_fk` 
-	FOREIGN KEY(`empid`) REFERENCES `employee`(`id`);
+	FOREIGN KEY(`emp_id`) REFERENCES `employee`(`id`);
 
-CREATE TABLE item_in_cart ( 
-	cartid     bigint(20) NOT NULL,
-	itemid     bigint(20) NOT NULL,
+
+
+CREATE TABLE `item_in_cart` ( 
+	cart_id     bigint(20) NOT NULL,
+	item_id     bigint(20) NOT NULL,
 	spent_count	bigint(20) NULL,
 	spent_money	bigint(20) NULL,
 	PRIMARY KEY (`cartid`, `itemid`)
 	);
+ALTER TABLE `item_in_cart` ADD CONSTRAINT `item_in_cart_user_fk` 
+	FOREIGN KEY(`cart_id`) REFERENCES `cart`(`id`);
+
+ALTER TABLE `item_in_cart` ADD CONSTRAINT `item_in_cart_emp_fk` 
+	FOREIGN KEY(`item_id`) REFERENCES `item`(`id`);
+
+
+
+CREATE TABLE `notification` (
+	id bigint(20) AUTO_INCREMENT NOT NULL, 
+	user_id bigint(20) NOT NULL,
+	action varchar(64) NOT NULL,
+	msg varchar(512) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `notification` ADD CONSTRAINT `notification_user_fk` 
+	FOREIGN KEY(`user_id`) REFERENCES `user`(`id`);
 
