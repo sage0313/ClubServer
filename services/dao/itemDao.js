e76d2f93879fd19eac8986380f3093e394bde3a6
SungKyu Sim <simsungkyu@gmail.com>
2014-05-06 01:21:04 +0900

    bug fix #1

--- services/dao/itemDao.js
+++ services/dao/itemDao.js
@@ -26,7 +26,7 @@ exports.selectItemById = function(idarg, conn, callback){
 exports.selectHasItemsByEmployee = function(eidarg, conn, callback){
 	var eid = conn.escape(eidarg);
 	var query= "select a.id , b.name , b.description, b.type, b.money , a.cnt  ";
-	query += " from (select d.item_id id , sum(d.spend_count) cnt from cart c, item_in_cart d  where c.emp_id = 1  and c.id = d.cart_id group by d.item_id) a , item b ";
+	query += " from (select d.item_id id , sum(d.spend_count) cnt from cart c, item_in_cart d  where c.emp_id = "+eid+"  and c.id = d.cart_id group by d.item_id) a , item b ";
 	query +=" where a.id = b.id ";
 	conn.query(query,function(err, rows, fields) {
 		callback(err,rows);
