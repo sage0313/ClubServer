06bed11820621dbfb78554c70eba35643b1de9b3
SungKyu Sim <simsungkyu@gmail.com>
2014-05-12 10:18:21 +0900

    update for admin init

--- public/js/admin.js
+++ public/js/admin.js
@@ -16,3 +16,19 @@ window.onload = function(){
 		}
 	});
 }
+
+var onchange_search_type = function(){
+	var search_type = $("#search_type_select option:selected").val();
+	console.log(search_type);
+	if(parseInt(search_type)==1){
+		var str = "<form class='form-inline' > aaaaaaaaaaaaaa<br/>bbbbbbbbbbb</form>";
+		$("#search_method_div").html(str);
+	}else{
+
+	}
+
+}
+
+var search1 = function(){
+	$("#result_table").html('');
+}
